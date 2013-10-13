8b24068196e0b698cb1078279bf59e44b85c90f5
Penn Su <pennsu@gmail.com>
2013-10-13 23:53:07 +0800

    Added questions to complete profiles

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,17 @@
-var app = angular.module('hackdate', ['ngRoute', 'ui.router', 'firebase']);
+var app = angular.module('hackdate', ['ngRoute', 'ui.bootstrap', 'ui.router', 'firebase']);
 
 app.value('hackDateURL', 'https://hackdate.firebaseio.com/');
+
+app.directive('selectize', function($timeout) {
+  return {
+    restrict: 'A',
+    link: function(Scope, element, attrs) {
+      $timeout(function() {
+        $(element).selectize(eval(attrs.selectize));
+      });
+    }
+  };
+});
 
 app.factory('Profiles', function(angularFireCollection, hackDateURL) {
   var ref = new Firebase(hackDateURL + 'profiles');
@@ -87,11 +98,15 @@ app.config(function($stateProvider, $urlRouterProvider) {
     .state('hackdate.profile', {
       url: "/profile",
       templateUrl: "partials/profile.html",
-      controller: function($scope, angularFire, hackDateURL, Profiles) {
+      controller: function($scope, $http, $timeout, angularFire, hackDateURL, Profiles) {
         console.log('accessing profile');
         console.log($scope.user);
         var profileUrl = hackDateURL + 'profiles/' + $scope.user.id;
         var ref = new Firebase(profileUrl);
+
+        $scope.sexes = ['M', 'F'];
+        $scope.relationship_status = ['Single', 'Married', 'Other'];
+        $scope.intentions = ['Relationship', 'Friendship', 'Business Partner'];
 
         $scope.remote = {};
         angularFire(ref, $scope, 'remote').
@@ -109,6 +124,111 @@ app.config(function($stateProvider, $urlRouterProvider) {
           $scope.reset = function() {
             $scope.profile = {};
           };
+
+          $scope.geolocate = function() {
+            if(navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                var latlng = position.coords.latitude+","+position.coords.longitude;
+                var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+latlng+'&sensor=true';
+
+                console.log(url);
+
+                $http.get(url).success(function(result) {
+                  console.log(result);
+                  // Could discuss on which components we need or let users to filter and to store
+                  // https://developers.google.com/maps/documentation/geocoding/#ReverseGeocoding
+                  $scope.profile.location = result.results[0].formatted_address;
+                });
+              });
+            } else {
+              console.log("Browser doesn't supports it, use other ways");
+            }
+          };
+
+          $scope.addQuestion = function() {
+            if(!$scope.profile.qas) {
+              $scope.profile.qas = [];
+            }
+
+            if(_.map($scope.profile.qas, function(qa) { return qa.question; }).indexOf($scope.new_question) > -1) {
+              alert('Question already exist!');
+            } else {
+              $scope.profile.qas.push({
+                type: 'user',
+                question: $scope.new_question,
+                answer: ''
+              });
+            }
+          };
+
+          $scope.removeQuestion = function(qa) {
+            var index = $scope.profile.qas.indexOf(qa);
+            if(index > -1) {
+              $scope.profile.qas.splice(index, 1);
+            }
+          };
+
+          $scope.setupQuestions = function() {
+            var defaultQAs = [
+              {
+                type: 'default',
+                question: 'Favorite food and why?',
+                answer: ''
+              },
+              {
+                type: 'default',
+                question: 'Most embarrassing story',
+                answer: ''
+              },
+              {
+                type: 'default',
+                question: 'Favorite memory',
+                answer: ''
+              },
+              {
+                type: 'default',
+                question: 'What do you inspire to do in life?',
+                answer: ''
+              },
+              {
+                type: 'default',
+                question: 'What are your biggest pet-peeves?',
+                answer: ''
+              },
+              {
+                type: 'default',
+                question: 'Who is the most important person in your life & why?',
+                answer: ''
+              },
+              {
+                type: 'default',
+                question: 'Thoughts about marriage/having a family?',
+                answer: ''
+              },
+              {
+                type: 'default',
+                question: 'One startup idea that you had (e.g. failed idea or just in general)',
+                answer: ''
+              },
+              {
+                type: 'default',
+                question: 'What is your most proudest moment?',
+                answer: ''
+              },
+              {
+                type: 'default',
+                question: 'What do you do for fun?',
+                answer: ''
+              }
+            ];
+
+            if(!$scope.profile.qas || $scope.profile.qas && $scope.profile.qas.length == 0) {
+              $scope.profile.qas = defaultQAs;
+            }
+          };
+
+          $scope.setupQuestions();
+
         }, function(reason) {
           console.log('error setting remote');
           console.log(reason);
